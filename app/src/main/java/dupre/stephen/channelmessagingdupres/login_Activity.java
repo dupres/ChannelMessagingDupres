a33c4410d57be9e97ce0bde0d04530539f5ced0f
dupres <stephen.dupre@hotmail.fr>
2017-03-10 16:49:24 +0100

    1.5

--- app/src/main/java/dupre/stephen/channelmessagingdupres/login_Activity.java
+++ app/src/main/java/dupre/stephen/channelmessagingdupres/login_Activity.java
@@ -1,17 +1,24 @@
 package dupre.stephen.channelmessagingdupres;
 
+import android.app.ActivityOptions;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
+import android.os.Handler;
 
 /**
  * Created by dupres on 06/02/2017.
@@ -31,6 +38,24 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
         boutonValider.setOnClickListener(this);
         identifiant = (EditText) findViewById(R.id.Identifiant);
         password = (EditText) findViewById(R.id.Password);
+
+        final View logo = findViewById(R.id.imageViewLogoWhite);
+        final Handler mHandlerTada = new Handler(); // android.os.handler
+        final Integer mShortDelay = 4000; //milliseconds
+        mHandlerTada.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.Tada).playOn(logo);
+                // Your code here
+                mHandlerTada.postDelayed(this, mShortDelay);
+            }
+        }, mShortDelay);
+        
+        final View tBox = findViewById(R.id.textViewQuote);
+        final Handler mHandlerQuote = new Handler();
+
+
+
+
     }
 
     @Override
@@ -57,7 +82,8 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
                     editor.commit();
 
                     Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
-                    startActivity(myIntent);
+                    View logo1 = findViewById(R.id.imageViewLogoWhite);
+                    startActivity(myIntent, ActivityOptions.makeSceneTransitionAnimation(login_Activity.this,logo1,"logo").toBundle());
                 }
                 else{
                     Toast.makeText(getApplicationContext(), "Erreur de connexion", Toast.LENGTH_SHORT).show();

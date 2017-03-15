85909505bbc965825b1b2a825399989fca11a8ae
dupres <stephen.dupre@hotmail.fr>
2017-03-15 11:46:47 +0100

    v1.6

--- app/src/main/java/dupre/stephen/channelmessagingdupres/login_Activity.java
+++ app/src/main/java/dupre/stephen/channelmessagingdupres/login_Activity.java
@@ -1,8 +1,11 @@
 package dupre.stephen.channelmessagingdupres;
 
+import android.annotation.TargetApi;
 import android.app.ActivityOptions;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -11,10 +14,15 @@ import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
+import com.dynamitechetan.flowinggradient.FlowingGradient;
+import com.dynamitechetan.flowinggradient.FlowingGradientClass;
 import com.google.gson.Gson;
 
 import java.util.HashMap;
@@ -30,6 +38,7 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
     private Button boutonValider;
     public static final String PREFS_NAME = "PrefsFiles";
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,10 +47,26 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
         boutonValider.setOnClickListener(this);
         identifiant = (EditText) findViewById(R.id.Identifiant);
         password = (EditText) findViewById(R.id.Password);
+        final TextView quote = (TextView) findViewById(R.id.textViewQuote);
+        final int[] quoteNb = {0};
+        final String[] quotes = new String[10];
+        quotes[0]="Chat with your friends !";
+        quotes[1]="Quote 1";
+        quotes[2]="Quote 2";
+        quotes[3]="Quote 3";
+        quotes[4]="Quote 4";
+        quotes[5]="Quote 5";
+        quotes[6]="Quote 6";
+        quotes[7]="Quote 7";
+        quotes[8]="Quote 8";
+        quotes[9]="Quote 9";
+
+
+
 
         final View logo = findViewById(R.id.imageViewLogoWhite);
         final Handler mHandlerTada = new Handler(); // android.os.handler
-        final Integer mShortDelay = 4000; //milliseconds
+        final Integer mShortDelay = 3000; //milliseconds
         mHandlerTada.postDelayed(new Runnable(){
             public void run(){
                 YoYo.with(Techniques.Tada).playOn(logo);
@@ -52,34 +77,65 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
         
         final View tBox = findViewById(R.id.textViewQuote);
         final Handler mHandlerQuote = new Handler();
+        final Integer mShortDelayQuote = 4000;
 
+        final Handler mHandlerQuote2 = new Handler();
+        final Integer mShortDelayQuote2 = 1000;
+
+        mHandlerQuote.postDelayed(new Runnable(){
+            public void run(){
+                YoYo.with(Techniques.SlideOutLeft).playOn(quote);
+
+                mHandlerQuote2.postDelayed(new Runnable(){
+                    public void run(){
+
+                        quoteNb[0] = quoteNb[0] +1;
+                        if (quoteNb[0] >9) {
+                            quoteNb[0]=0;
+                        }
+                        quote.setText(quotes[quoteNb[0]]);
+                        YoYo.with(Techniques.SlideInRight).playOn(quote);
+                    }
+                },mShortDelayQuote2);
+                mHandlerQuote.postDelayed(this, mShortDelayQuote);
+            }
+        },mShortDelayQuote);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.linearLayout_id);
+        FlowingGradientClass gradient = new FlowingGradientClass();
+        gradient.setBackgroundResource(R.drawable.translate).setTransitionDuration(4000).onLinearLayout(linearLayout).start();
 
 
 
     }
 
+    @TargetApi(Build.VERSION_CODES.N)
     @Override
     public void onClick(View v) {
         HashMap<String, String> params = new HashMap<String, String>();
         params.put("username",identifiant.getText().toString());
         params.put("password",password.getText().toString());
 
-        Downloader connexion = new Downloader(getApplicationContext(), params, "http://www.raphaelbischof.fr/messaging/?function=connect");
+        Animation animSlideLeft = AnimationUtils.loadAnimation(this,R.anim.slide_left);
+        findViewById(R.id.textViewQuote).startAnimation(animSlideLeft);
 
+        YoYo.with(Techniques.FadeOut).playOn(boutonValider);
+        final ProgressDialog progress = new ProgressDialog(this);
+        progress.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+        progress.setProgress(0);
+        Downloader connexion = new Downloader(getApplicationContext(), params, "http://www.raphaelbischof.fr/messaging/?function=connect");
         connexion.setListener(new OnDownloadCompleteListener() {
             @Override
             public void onDownloadComplete(String content) {
                 Gson gson = new Gson();
                 ConnectionReturn obj = gson.fromJson(content, ConnectionReturn.class);
-
                 if(obj.getResponse().equals("Ok")){
                     Toast.makeText(getApplicationContext(), "Connecté", Toast.LENGTH_SHORT).show();
-
                     SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
                     SharedPreferences.Editor editor = settings.edit();
                     editor.putString("accesstoken", obj.getAccesstoken());
-
                     editor.commit();
+                    progress.setProgress(50);
 
                     Intent myIntent = new Intent(getApplicationContext(),ChannelListActivity.class);
                     View logo1 = findViewById(R.id.imageViewLogoWhite);
@@ -90,6 +146,7 @@ public class login_Activity extends AppCompatActivity implements OnDownloadCompl
                 }
             }
         });
+        progress.setProgress(100);
         connexion.execute();
         Toast.makeText(getApplicationContext(), "Connexion en cours...", Toast.LENGTH_SHORT).show();
 
